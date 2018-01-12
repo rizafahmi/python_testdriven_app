212a92e5277b185bd291d53eda2fdccc13c65e9a
Riza Fahmi <rizafahmi@gmail.com>
2018-01-12 16:30:08 +0700

    Test setup

--- users-service/project/config.py
+++ users-service/project/config.py
@@ -5,6 +5,7 @@ class BaseConfig:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
@@ -14,7 +15,7 @@ class DevelopmentConfig(BaseConfig):
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
-    Testing = True
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
---
+++ users-service/project/tests/base.py
@@ -0,0 +1,17 @@
+from flask_testing from TestCase
+
+from project import app, db
+
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
