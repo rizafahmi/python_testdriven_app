321e3542c40adf8bd038a604ed215ea793868619
Riza Fahmi <rizafahmi@gmail.com>
2018-01-17 11:19:32 +0700

    JWT setup

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -58,3 +58,14 @@ class TestUserModel(BaseTestCase):
         self.assertEqual(user.email, 'test@test.com')
         self.assertTrue(user.active)
         self.assertTrue(user.password)
+
+    def test_encode_auth_token(self):
+        user = add_user('justtest', 'test@test.com', 'test')
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+
+    def test_decode_auth_token(self):
+        user = add_user('justtest', 'test@test.com', 'test')
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertEqual(User.decode_auth_token(auth_token), user.id)
