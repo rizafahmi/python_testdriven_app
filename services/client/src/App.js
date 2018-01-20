a62d61646844c231a6bfc375bab9b0a5d77da178
Riza Fahmi <rizafahmi@gmail.com>
2018-01-20 21:26:52 +0700

    React bootstrap

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom'
 import UsersList from './components/UsersList'
 import AddUser from './components/AddUser'
 import About from './components/About'
+import NavBar from './components/NavBar'
+import Form from './components/Form'
 
 class App extends React.Component {
   constructor (props) {
@@ -12,7 +14,13 @@ class App extends React.Component {
     this.state = {
       users: [],
       username: '',
-      email: ''
+      email: '',
+      title: 'TestDriven.io',
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
     }
   }
   componentDidMount () {
@@ -26,7 +34,7 @@ class App extends React.Component {
           users: res.data.data.users
         })
       })
-      .catch(err => console.log(err))
+      .catch(() => {})
   }
   handleAddUser (event) {
     event.preventDefault()
@@ -52,32 +60,52 @@ class App extends React.Component {
   }
   render () {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-4'>
-            <br />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={() => (
-                  <div>
-                    <h1>All Users</h1>
-                    <hr />
-                    <br />
-                    <AddUser
-                      handleAddUser={this.handleAddUser.bind(this)}
-                      handleInputChange={this.handleInputChange.bind(this)}
-                      username={this.state.username}
-                      email={this.state.email}
+      <div>
+        <NavBar title={this.state.title} />
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <br />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={() => (
+                    <div>
+                      <h1>All Users</h1>
+                      <hr />
+                      <br />
+                      <AddUser
+                        handleAddUser={this.handleAddUser.bind(this)}
+                        handleInputChange={this.handleInputChange.bind(this)}
+                        username={this.state.username}
+                        email={this.state.email}
+                      />
+                      <br />
+                      <UsersList users={this.state.users} />
+                    </div>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route
+                  exact
+                  path='/register'
+                  render={() => (
+                    <Form
+                      formType={'Register'}
+                      formData={this.state.formData}
                     />
-                    <br />
-                    <UsersList users={this.state.users} />
-                  </div>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-            </Switch>
+                  )}
+                />
+                <Route
+                  exact
+                  path='/login'
+                  render={() => (
+                    <Form formType={'Login'} formData={this.state.formData} />
+                  )}
+                />
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
---
+++ services/client/src/components/Form.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Form = props => {
+  return (
+    <div>
+      <h1>{props.formType}</h1>
+      <form onSubmit={event => props.handleUserFormSubmit(event)}>
+        {props.formType === 'Register' && (
+          <div className='form-group'>
+            <input
+              className='form-control input-lg'
+              name='username'
+              type='text'
+              placeholder='Enter a username'
+              required
+              value={props.formData.username}
+              onChange={props.handleFormChange}
+            />
+          </div>
+        )}
+        <div className='form-group'>
+          <input
+            className='form-control input-lg'
+            name='email'
+            type='email'
+            placeholder='Enter an email address'
+            required
+            value={props.formData.email}
+            onChange={props.handleFormChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            className='form-control input-lg'
+            name='password'
+            type='password'
+            placeholder='Enter a password'
+            required
+            value={props.formData.password}
+            onChange={props.handleFormChange}
+          />
+        </div>
+        <input
+          type='submit'
+          className='btn btn-warning btn-lg btn-block'
+          value='Submit'
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Form
